69b1a2229b87cca9e39f1b98da8e3d4acf5f2f0e
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-05-26 11:23:17 -0600

    Iteration 2

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+require "rspec"
+require "./lib/hospital"
+
+RSpec.describe "Hospital" do
+  it "has a name" do
+    hospital = Hospital.new("Seattle Grace", "Richard Webber", [])
+    expect(hospital.name).to eq("Seattle Grace")
+  end
+
+it "has a chief of surgery" do
+    hospital = Hospital.new("Seatlle Grace", "Richard Webber", [])
+    expect(hospital.chief_of_surgery).to eq("Richard Webber")
+  end
+
+  it "has doctors" do
+    hospital = Hospital.new("Seattle Grace", "Richard Webber", [])
+    expect(hospital.doctors).to eq([])
+
+  end
+
+end
